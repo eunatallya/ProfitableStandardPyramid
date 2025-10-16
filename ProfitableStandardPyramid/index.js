996317262038c639c7b4a261cea434499a234fd7
eunatallya <geh.natally45@gmail.com>
2025-10-16 16:21:07 -0300

    Fix syntax error in app.listen callback

--- ProfitableStandardPyramid/index.js
+++ ProfitableStandardPyramid/index.js
@@ -39,6 +39,6 @@ app.get('/db-status', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () = {
+app.listen(PORT, () =>{
   console.log('Servidor Express iniciado e ouvindo na porta ${PORT}');
 });
