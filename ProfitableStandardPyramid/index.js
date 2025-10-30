826f9150f1e020079ea575a95c2090a2ea5bbde2
eunatallya <geh.natally45@gmail.com>
2025-10-30 20:28:21 -0300

    Update index.js

--- ProfitableStandardPyramid/index.js
+++ ProfitableStandardPyramid/index.js
@@ -45,7 +45,7 @@ aplicativo.pegar('/adicionar-senha-hash', async (req, res) => {
         const cliente = await piscina.connect();
         
         const sql = `
-            ALTER TABLE usuarios
+            ALTER TABLE profissional.html
             ADD COLUMN senha_hash VARCHAR(255);
         `;
 
